50ef692050de078fe5069876b4038082f2cce7ee
webber0928 <bird23074035@gmail.com>
2023-11-21 14:12:58 +0800

    Change: 修改時區

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -103,9 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'zh-Hant'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
