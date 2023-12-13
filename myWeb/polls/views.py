65b5eb6499506d1308cc848ff3627a33948b49fe
webber0928 <bird23074035@gmail.com>
2023-12-13 15:26:12 +0800

    Feature:new 404 page

--- myWeb/polls/views.py
+++ myWeb/polls/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
-
+    
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
